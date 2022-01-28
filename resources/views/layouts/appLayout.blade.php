a4c28eafe60961c69b7b963f70255e64cb53289d
MissaelLopez <missaelnj8@gmail.com>
2022-01-28 15:53:57 -0600

    Fixed styles

--- resources/views/layouts/appLayout.blade.php
+++ resources/views/layouts/appLayout.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-    <script src={{ URL::asset('js/tailwindConfig.js') }}></script>
+    <script src="https://wolf-tech.herokuapp.com/js/tailwindConfig.js"></script>
     <title>Tech</title>
     <style>
       body {
@@ -107,6 +107,6 @@
       </main>
     </div>
   </div>
-  <script src={{ URL::asset('js/functions.js') }}></script>
+  <script src="https://wolf-tech.herokuapp.com/js/functions.js"></script>
 </body>
 </html>
--- resources/views/layouts/authLayout.blade.php
+++ resources/views/layouts/authLayout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src={{ URL::asset('js/tailwindConfig.js') }}></script>
+    <script src="https://wolf-tech.herokuapp.com/js/tailwindConfig.js"></script>
     <title>Wolf Games</title>
 </head>
 <body>
@@ -24,6 +24,6 @@
             </div>
         </div>
     </div>
-    <script src={{ URL::asset('js/functions.js') }}></script>
+    <script src="https://wolf-tech.herokuapp.com/js/functions.js"></script>
 </body>
 </html>
