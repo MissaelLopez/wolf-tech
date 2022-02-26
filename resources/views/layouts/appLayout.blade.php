1616a87f6671cd49251e4e200d66a97151fd1e2e
MissaelLopez <missaelnj8@gmail.com>
2022-02-26 15:03:30 -0600

    assets

--- resources/views/layouts/appLayout.blade.php
+++ resources/views/layouts/appLayout.blade.php
@@ -9,7 +9,8 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>    
-    <script src="https://wolf-tech.herokuapp.com/js/tailwindConfig.js"></script>     
+    <script src="https://wolf-tech.herokuapp.com/js/tailwindConfig.js"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>  
     <title>{{ $title ?? 'Game News' }}</title>
     <style>
       body {
