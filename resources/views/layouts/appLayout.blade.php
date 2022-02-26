9ec74b8b59603fc779ebb04935ba09a3f52d594e
MissaelLopez <missaelnj8@gmail.com>
2022-02-26 14:53:09 -0600

    assets

--- resources/views/layouts/appLayout.blade.php
+++ resources/views/layouts/appLayout.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
@@ -114,7 +115,10 @@
     </div>
   </div>
   <script src="https://wolf-tech.herokuapp.com/js/functions.js"></script>
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>  
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
   <!-- <script src="{{ asset('js/functions.js') }}"></script> -->
 </body>
 </html>
